b13d53c7b087e2709edd8f21a4b16e94b88251eb
mikerock33212 <mikerock332@gmail.com>
2022-02-09 19:22:33 +0800

    finished day 24 files, paths, etc

--- Mail Merge Project Start/main.py
+++ Mail Merge Project Start/main.py
@@ -2,6 +2,24 @@
 #for each name in invited_names.txt
 #Replace the [name] placeholder with the actual name.
 #Save the letters in the folder "ReadyToSend".
+
+
+def replace(name_1):
+    """replace a given name in a file with your expected name"""
+    with open('../Mail Merge Project Start/Input/Letters/starting_letter.txt', mode='r') as f:
+        text = f.read()
+    with open(f'../Mail Merge Project Start/Output/ReadyToSend/letter_for_{name}', mode='w') as f:
+        f.write(text.replace('[name]', name_1))
+
+
+with open('../Mail Merge Project Start/Input/Names/invited_names.txt', mode='r')as f:
+    lines = f.readlines()
+    clean_list = [line.strip() for line in lines]
+for name in clean_list:
+    replace(name)
+
+
+
     
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
